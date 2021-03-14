a3b4cb9710fef9513edc11bebb3accd774aadba4
oladotun <oladotunopasina@gmail.com>
2021-03-13 20:31:08 -0500

    Updated the logo and name to show seeksco

--- resources/views/frontend/layouts/template_03.blade.php
+++ resources/views/frontend/layouts/template_03.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <title> SeeksCo - User Reviews</title>
     {!! SEO::generate() !!}
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/jost/stylesheet.css')}}"/>
