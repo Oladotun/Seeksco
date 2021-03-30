382edf805f35c80ab0db0973b1dea792376bdb59
oladotun <oladotunopasina@gmail.com>
2021-03-30 11:40:48 -0500

    added segment analytics to website

--- resources/views/frontend/layouts/template.blade.php
+++ resources/views/frontend/layouts/template.blade.php
@@ -32,6 +32,12 @@
     <meta name="csrf-token" content="{{csrf_token()}}"/>
     <script>
         var app_url = window.location.origin;
+
+        !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="nVUvHBaPCqCbRVGmv8hmdRt0DcNC2XiF";analytics.SNIPPET_VERSION="4.13.2";
+        analytics.load("nVUvHBaPCqCbRVGmv8hmdRt0DcNC2XiF");
+        analytics.page();
+        
+        }}();
     </script>
     @stack('style')
 </head>
